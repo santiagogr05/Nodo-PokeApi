6fc22b0f93e08916734c0d3183ab2c3dcf59493d
Appitoriadev <contact@appitoria.dev>
2024-11-06 17:34:57 -0500

    Add initial project files including LICENSE, README, HTML, CSS, and JavaScript for Pokémon app

---
+++ README.md
@@ -0,0 +1,30 @@
+# Poke Api Test
+
+Holi 🤗
+
+En este workshop vamos a crear una aplicación basada en la [API de Pokemon.](https://pokeapi.co/)
+
+Se tocarán temas básicos sobre:
+
+- HTML semántico
+- CSS con BEM
+- JS Vanilla (selectores, async/await, eventListeners)
+
+## Instalación. 🧪
+
+Para instalar este repo no necesitas ninguna dependencia, basta con que clones el repositorio así
+
+```shell
+git clone https://github.com/appitoriadev/Poke-Api-Test.git
+```
+
+Esto descargará los archivos necesarios para empezar a trabajar.
+
+## Qué necesitas? 😎
+
+- Un editor de código (IDE), recomendamos VSCode
+- Conexión a internet
+- LiveServer: Extensión para visualizar los cambios en tu código de manera instantánea
+- Muchas ganas de aprender!
+
+Si tienes alguna duda, por pequeña que sea, pregunta a tu mentor que está aquí para ayudarte 😉
---
+++ index.js
@@ -0,0 +1,95 @@
+// Ahora que tenemos nuestro HTML y CSS, es hora de darle vida con JavaScript <3
+
+// 1️⃣. Seleccionar los elementos HTML que vamos a utilizar:
+// - Imagen de los pokemon
+// - Stats de cada uno
+// 🤓 Pista: revisa el método document.querySelector()
+
+// Selectores para el Pokemon 1
+
+
+// Selectores para el Pokemon 2
+
+
+// 2️⃣. Miremos ahora la API de Pokemon :)
+// - Haz un llamado a la URL https://pokeapi.co/api/v2/pokemon/ y analiza cómo devuelve su respuesta
+// La API retorna un pokemon https://pokeapi.co/api/v2/pokemon/{ID} si se provee un ID al final.
+// 🤓 Pista: Para enfrentar 2 pokemones aleatores, necesitamos hacer 2 llamados a la API con 2 n´¨úmeros aleatorios entre el 1 y el 900
+
+
+// 3️⃣ - Crear una función que genere un número random entre 1 y 900.
+// Puedes usar esta: 👩🏻‍💻
+/* const getRandomNumber = (numMin, numMax) => {
+  return Math.floor(Math.random() * (numMax - numMin + 1) + numMin);
+}; */
+
+// 4️⃣ - Asignar un número random al ID de los que serán nuestros pokemons
+// Declara 2 variables para cada pokemon y guarda los números que retorna la funci´øn en ellos
+
+// 🤓 Pista: algo como ... const poke1ID = getRandomNumber(1, 900);
+
+// 5️⃣ - Crear una función para traer (fetch) data de la API
+// Dale una mirada a la función fetch -> https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+// Recuerda la URL de la API https://pokeapi.co/api/v2/pokemon/${pokeID}
+
+//Puedes usar esta: 👩🏻‍💻
+/* const getPokemon = async (pokeID) => {
+  const response = await fetch(` https://pokeapi.co/api/v2/pokemon/${pokeID}`);
+  const data = await response.json();
+  return data;
+}; */
+
+// 6️⃣ - Vamos a crear los pokemons en la función createPokemons.
+// Primero Haz varias pruebas a las API para examinar bien qué devuelve, esa data
+// será necesaria para popular nuestros elementos HTML
+// 🤓 Pista: - Crea una función asíncrona que reciba los 2 ID de los pokemon, es decir los números que obtenemos de llamar la función random
+//           - Haz una llamada a la API por cada pokemon, guarda los datos que te devuelve en dos variables: pokemon1 y pokemon2
+//           - Toma los elementos HTML que seleccionamos más arriba y utiliza su propiendad innerHTML para añadir la info que necesitamos de la API
+
+
+
+
+// 🎁 Bonus! - Vamos a crear la función fightPokemons que permitirá que los pokemons interactúen y peleen
+
+  // 1. Seleccionar los datos que ahora tenemos en el HTML y que trajimos desde la API: para ambos pokemon: HP, attack, defense y name.
+
+
+  // 2. Crear una función que calcule el daño hecho a cada pokemon. Necesitamos el poder del atacante y la defensa y los HP del que defiende
+  // - Calcular el daño restando el ataque de la defensa, con esto definimos si el pokemon sufrió daño.
+  // - Calcular los nuevos HP: Si la defensa es menor a 0, significa que el ataque logró perforarla e hizo daño, en este caso vamos a restar el daño de los HP, si no, la HP debe quedar igual pues no hubo da˜ño
+  // En esta función vamos a devolver la nueva HP del pokemon atacado y además el da˜ñó que sufrió. - Luego vamos a necesitar estos datos -
+
+
+  // 3. Narrar la batalla ;). Para esto vamos a usar el elemento modal__text, aquí vamos a ir llenando su innerHTML.
+  // Empecemos con el Pokemon 1.
+
+
+
+  // Ahora calculemos el daño que le hizo a pokemon2 y cuánta vida le queda, usemos la función de calcular daño
+
+
+
+  // Vamos a narrar qué pasó en este ataque.Si el pokemon 1 tiene un ataque mayor a la denfesa del pokemon 2, debemos narrar que logra perforar su defensa
+  // y describir cuánto daño recibió y cuáles son ahora sus puntos de vida
+  // Si el ataque del pokemon 1 no es mayor que la denfesa del pokemon 2, significa que no logra perforarla y por lo tanto no hay daño.
+
+
+  // Ahora el Pokemon2, mismo procedimiento.
+
+
+  // Definamos el ganador que sería el más daño haya hecho al otro pokemon.
+  // Recordemos que los puntos de daño son negativos!!
+  // - Si el daño recibido por pokemon 2 es menor al de pokemon 1, (es decir un mayor número negativo), significa que pokemon 1 hizo más daño, por lo tanto es el gandor!
+  // - En caso de que sea menor el daño de pokemon 1, significa que pokemon 2 es el gandor
+  // - El último caso posible es que ambos hayan recibido el mismo daño, en ese caso sería un empate.
+
+
+
+// 7️⃣ - Vamos a practicar eventos en JS, de esta manera vamos a poder controlar cuándo traer pokemons desde la interfaz
+// Nuestra función fetch va a traer pokemons cada que el código es ejecutado, es decir cuando la página se recarga
+// Vamos a añadir un botón que recargue la página en cada click, así podemos obtener nuevos pokemons random cada vez.
+// 🤓 Pista: - Seleccionar el elmento HTML del botón
+//           - Llamar a la función createPokemons solo cuando se dé click a ese botón (lee sobre eventListeners https://www.w3schools.com/js/js_htmldom_eventlistener.asp )
+// 🕵🏻‍♀️ En nuestra app tenemos 3 botones. El de Catch!, Fight! y el que OK! que aparece en el modal cuando pelean los pokemons
+// Cada botón tendrá atado un eventListener que vamos a construir juntos ❤️
+
